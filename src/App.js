563fbfcd19e0d109bee779ca0334a6b38bec7083
Joao <joao.g.leal@gmail.com>
2020-04-09 19:50:57 -0300

    Adiciona e remove repositórios da lista

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepo] = useState([])
+
+  // Buscando no backend os repositórios já cadastrados
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepo(response.data)
+    });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    // Adiciona repositórios à lista
+    const response = await api.post('/repositories', {
+      url: "https://github.com/Rocketseat/repo2",
+      title: `Repo ${Date.now()}`,
+      techs: ["Node", "Express", "TypeScript"]
+    });
+
+    setRepo([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    // Remove repositórios cadastrados
+    await api.delete(`/repositories/${id}`);
+
+    setRepo(repositories.filter(repository => repository.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => 
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>)}
+
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
